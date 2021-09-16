9b848a7e2aaf4afa4c72bee1b99f8f60e8e3be51
Grigala <76661178+R-Grigala@users.noreply.github.com>
2021-09-16 16:38:18 +0400

    Add files via upload

---
+++ Swift_Digital/4_S_D.py
@@ -0,0 +1,19 @@
+#function to count numbers of climbing options
+def findStep(n) :
+
+    if (n == 1 or n == 0) :
+        return 1
+
+    elif (n == 2) :
+        return 2
+
+    else :
+        return findStep(n - 2) + findStep(n - 1)
+ 
+ 
+# Driver code
+
+n = int(input("Enter number of stairs: "))
+# If n = 3 then the number of climbing options is 3
+# because {1 + 1 + 1 = 3; 2 + 1 = 3; 1 + 2 = 3}
+print("Number of options for climbing stairs: ",findStep(n))
