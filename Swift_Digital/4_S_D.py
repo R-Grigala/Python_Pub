d879e154d24bb52ec55dc19f896234846a60ed20
R-Grigala <r.grigalashvili777@gmail.com>
2021-10-06 22:24:23 +0400

    origin master

--- Swift_Digital/4_S_D.py
+++ Swift_Digital/4_S_D.py
@@ -17,3 +17,4 @@ n = int(input("Enter number of stairs: "))
 # If n = 3 then the number of climbing options is 3
 # because {1 + 1 + 1 = 3; 2 + 1 = 3; 1 + 2 = 3}
 print("Number of options for climbing stairs: ",findStep(n))
+#R_Grigala
