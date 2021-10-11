35ff797a68aaa3eb9bbf95f964156192b2bcc5ef
Grigala <76661178+R-Grigala@users.noreply.github.com>
2021-10-11 23:01:58 +0400

    Delete P_work_1.py

--- Work/P_work_1.py
+++
@@ -1,7 +0,0 @@
-def toFahrenheit(cel):
-    return float((cel * 1.8) + 32)
-
-
-celsius = float(input("Temperature value in degree Celsius: "))
-print(celsius, "degree Celsius is equal", toFahrenheit(celsius), "Fahrenheit")
-
